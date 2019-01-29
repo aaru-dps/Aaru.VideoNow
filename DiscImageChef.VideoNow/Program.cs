a0b7a541d99b1376ddeb64bef84d442ea5d4aa92
Natalia Portillo <claunia@claunia.com>
2019-01-29 18:16:23 +0000

    Initial commit.

---
+++ DiscImageChef.VideoNow/Program.cs
@@ -0,0 +1,65 @@
+﻿// /***************************************************************************
+// The Disc Image Chef
+// ----------------------------------------------------------------------------
+//
+// Filename       : Program.cs
+// Author(s)      : Natalia Portillo <claunia@claunia.com>
+//
+// Component      : VideoNow analyzing and decoding tools.
+//
+// --[ Description ] ----------------------------------------------------------
+//
+//     Main program loop.
+//
+// --[ License ] --------------------------------------------------------------
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as
+//     published by the Free Software Foundation, either version 3 of the
+//     License, or (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// ----------------------------------------------------------------------------
+// Copyright © 2011-2019 Natalia Portillo
+// ****************************************************************************/
+
+using System;
+using System.Reflection;
+
+namespace DiscImageChef.VideoNow
+{
+    static class MainClass
+    {
+        static string                                AssemblyCopyright;
+        static string                                AssemblyTitle;
+        static AssemblyInformationalVersionAttribute AssemblyVersion;
+
+        public static void Main(string[] args)
+        {
+            object[] attributes = typeof(MainClass).Assembly.GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+            AssemblyTitle = ((AssemblyTitleAttribute)attributes[0]).Title;
+            attributes    = typeof(MainClass).Assembly.GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
+            AssemblyVersion =
+                Attribute.GetCustomAttribute(typeof(MainClass).Assembly, typeof(AssemblyInformationalVersionAttribute))
+                    as AssemblyInformationalVersionAttribute;
+            AssemblyCopyright = ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
+
+            PrintCopyright();
+        }
+
+        static void PrintCopyright()
+        {
+            Console.WriteLine("{0} {1}", AssemblyTitle, AssemblyVersion?.InformationalVersion);
+            Console.WriteLine("{0}",     AssemblyCopyright);
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+DiscImageChef v4.5.99.1693
+
+Disc Image Chef (because "swiss-army-knife" is used too much)
+
+Copyright © 2011-2019 Natalia Portillo <claunia@claunia.com>
+
+DiscImageChef.VideoNow is an analyzing and conversion tool for Tiger and Hasbro VideoNow and VideoNow Color formats.
+
+Part of [DiscImageChef](https://github.com/discimagechef/DiscImageChef)
